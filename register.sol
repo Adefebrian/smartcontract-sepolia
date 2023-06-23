c0bbc05f531c601319ccd4102534187984764952
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2023-06-23 10:25:23 +0700

    Create register.sol

---
+++ register.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Register {
+    string public github;
+    address public owner;
+    
+    struct Referral {
+        address referralAddress;
+        string referralString;
+    }
+    
+    Referral[] public referrals;
+    
+    constructor() {
+        github = "adefebrian";
+        owner = 0x91bf8F4c5Bc370C906Ae2609D42193A20a8177f0;
+    }
+    
+    function addReferral(address _referralAddress, string memory _referralString) external {
+        require(msg.sender == owner, "Only the owner can add referrals.");
+        referrals.push(Referral(_referralAddress, _referralString));
+    }
+    
+    function totalReferrals() public view returns (uint256) {
+        return referrals.length;
+    }
+}
